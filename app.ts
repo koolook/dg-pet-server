d92e0a93040f02755a94936b138494ec30235b8b
Eugene Lukin <eugene.lukin@gmail.com>
2025-07-31 04:42:26 +0300

    Added Mongodb

--- app.ts
+++ app.ts
@@ -1,12 +1,38 @@
-    import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
+import { MongoClient } from 'mongodb';
 
-    const app: Application = express();
-    const port: number = 4000;
+const app: Application = express();
+const port: number = 4000;
 
-    app.get('/', (req: Request, res: Response) => {
-        res.send('Hello from Express with TypeScript!');
-    });
+let mongoClient: MongoClient;
 
-    app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
-    });
+app.get('/', (req: Request, res: Response) => {
+    res.send('>>> Hello from Express with TypeScript!');
+});
+
+app.get('/about', async (req: Request, res: Response) => {
+    const db = mongoClient.db('pet-base');
+    const data = await db.collection('messages').find().toArray();
+
+    console.log(JSON.stringify(data));
+    res.send(JSON.stringify(data));
+});
+
+async function start() {
+    try {
+        console.log('Connect DB!');
+        mongoClient = new MongoClient('mongodb://mongodb:27017/');
+        await mongoClient.connect();
+        console.log('Connected');
+
+        app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`);
+        });
+
+    } catch (error) {
+        console.log('Server Error', (error as Error).message)
+        process.exit(1)
+    }
+}
+
+start();

2624b6a91a60c4915b35e68c5a3049fe9dd2ca7c
Eugene Lukin <eugene.lukin@gmail.com>
2025-08-06 03:44:00 +0300

    Added simple validation to signup endpoint

--- authController.ts
+++ authController.ts
@@ -2,10 +2,19 @@ import { Request, Response } from 'express'
 import bcrypt from 'bcryptjs'
 
 import UserSchema from './models/User'
+import { Result, validationResult } from 'express-validator'
 
 class AuthController {
   signUp = async (req: Request, res: Response) => {
     try {
+      // handle validation results
+      const result: Result = validationResult(req)
+      if (!result.isEmpty()) {
+        return res.status(400).json({
+          message: 'Invalid user data provided',
+          result,
+        })
+      }
       const { login, password } = req.body
 
       // check user exists
--- authRouter.ts
+++ authRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
+import { check } from 'express-validator'
 
 import controller from './authController'
 
 const router = Router()
 
-router.post('/signup', controller.signUp)
+router.post(
+  '/signup',
+  [
+    check('login', 'User name can not be blank').notEmpty(),
+    check('password', 'Password length must be 5-10 symbols').isLength({ min: 5, max: 10 }),
+  ],
+  controller.signUp
+)
 router.post('/login', controller.login)
 router.get('/test', controller.test)
 
